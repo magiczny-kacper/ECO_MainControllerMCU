3907c1bff6e96fd284e85a2131a8e2b8c80de076
magiczny-kacper <kapibrv97@gmail.com>
2020-09-24 22:52:01 +0200

    Making things work again

--- Core/Src/CLI/CLI.h
+++ Core/Src/CLI/CLI.h
@@ -14,5 +14,6 @@
 
 void CLI_Init (void);
 BaseType_t xCLI_EreaseEE( char *pcWriteBuffer, size_t xWriteBufferLen, const int8_t *pcCommandString );
-
+BaseType_t xCLI_ShowConfig( char *pcWriteBuffer, size_t xWriteBufferLen, const int8_t *pcCommandString );
+BaseType_t xCLI_GetSystemStatus( char *pcWriteBuffer, size_t xWriteBufferLen, const int8_t *pcCommandString );
 #endif /* SRC_CLI_CLI_H_ */
